a47eadb98802b45d179ad03b1af4e80311fdb2d2
tanny <tanny.bao@gmail.com>
2018-03-13 17:02:50 +0800

    init commit

---
+++ mylibrary/src/main/java/com/tanny/mylibrary/MyViewPager.java
@@ -0,0 +1,20 @@
+package com.tanny.mylibrary;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+/**
+ * Created by tanny on 2018/3/13.
+ */
+
+public class MyViewPager extends ViewPager {
+
+    public MyViewPager(Context context) {
+        super(context);
+    }
+
+    public MyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
